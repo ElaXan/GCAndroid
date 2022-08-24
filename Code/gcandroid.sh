9302e5082b0102fae814adb4a0274700ff83d6b0
ZElaXan <107149635+ElaXan@users.noreply.github.com>
2022-08-24 15:34:53 +0700

    Add download version Resources

--- Code/gcandroid.sh
+++ Code/gcandroid.sh
@@ -602,8 +602,49 @@ installMongodb() {
 }
 
 Install_Grasscutter_process() {
+    if ! command -v unzip &> /dev/null; then
+        echo "${GC}Installing Unzip...${WC}"
+        sudo apt install unzip -y &> /dev/null
+    fi
     credit_hah
     cd $HOME || exit 1
+    if [[ $Install_Grasscutter_Resources = "Koko-Boya" ]]; then
+        echo "${GC}Choose Version...${WC}"
+        echo "${CCB}1. 3.0"
+        echo "2. 2.8"
+        echo "0. Back/Cancel"
+        echo
+        echo -n "Enter input : ${CCB}"
+        read Install_Grasscutter_process_input
+        case $Install_Grasscutter_process_input in
+            "1" ) Install_Grasscutter_Resources_Version="3.0";;
+            "2" ) Install_Grasscutter_Resources_Version="2.8";;
+            "0" ) main_menu;;
+            * ) echo "${RC}Wrong input!${WC}"
+        esac
+    elif [[ $Install_Grasscutter_Resources = "Yuuki" ]]; then
+        echo "${GC}Choose Version...${WC}"
+        echo "${CCB}1. 3.0"
+        echo "2. 2.8"
+        echo "3. 2.7"
+        echo "4. 2.6"
+        echo "0. ${RC}Back/Cancel${WC}"
+        echo
+        echo -n "Enter input : ${CCB}"
+        read Install_Grasscutter_process_input
+        case $Install_Grasscutter_process_input in
+            "1" ) Install_Grasscutter_Resources_Version="3.0";;
+            "2" ) Install_Grasscutter_Resources_Version="2.8";;
+            "3" ) Install_Grasscutter_Resources_Version="2.7";;
+            "4" ) Install_Grasscutter_Resources_Version="2.6";;
+            "0" ) main_menu;;
+            * ) echo "${RC}Wrong input!${WC}"; sleep 1s; Install_Grasscutter_process;;
+        esac
+    fi
+    credit_hah
+    echo "${CCB}Resources : ${GC}$Install_Grasscutter_Resources${WC}"
+    echo "${CCB}Version Resources : ${GC}$Install_Grasscutter_Resources_Version${WC}"
+    echo "====================================="
     echo "${GC}Install Java JDK 17${WC}"
     sleep 1s
     sudo apt install openjdk-17-jdk -y &> /dev/null
@@ -611,20 +652,39 @@ Install_Grasscutter_process() {
         rm -rf $HOME/Grasscutter
     fi
     sleep 1s
-    cd $HOME || exit
+    cd $HOME || exit 1
     if ! command -v git &> /dev/null; then
         echo "${GC}Install unzip...${WC}"
         sudo apt install unzip -y &> /dev/null
     fi
     echo "${GC}Download Grasscutter and Resources${WC}"
-    git clone https://github.com/Grasscutters/Grasscutter
+    git clone https://github.com/Grasscutters/Grasscutter.git
     echo "${GC}Download Resources...${WC}"
     sleep 1s
-    git clone https://github.com/Koko-boya/Grasscutter_Resources
+    cd $HOME || exit 1
+    if [[ $Install_Grasscutter_Resources = "Koko-Boya" ]]; then
+        if [[ $Install_Grasscutter_Resources_Version = "3.0" ]]; then
+            wget https://github.com/Koko-boya/Grasscutter_Resources/archive/refs/heads/3.0.zip -O zResources.zip -q --show-progress
+        elif [[ $Install_Grasscutter_Resources_Version = "2.8" ]]; then
+            wget https://github.com/Koko-boya/Grasscutter_Resources/archive/refs/heads/2.8.zip -O zResources.zip -q --show-progress
+        fi
+    elif [[ $Install_Grasscutter_Resources = "Yuuki" ]]; then
+        if [[ $Install_Grasscutter_Resources_Version = "3.0" ]]; then
+            wget https://gitlab.com/yukiz/GrasscutterResources/-/archive/3.0/GrasscutterResources-3.0.zip -O zResources.zip -q --show-progress
+        elif [[ $Install_Grasscutter_Resources_Version = "2.8" ]]; then
+            wget https://gitlab.com/yukiz/GrasscutterResources/-/archive/2.8/GrasscutterResources-2.8.zip -O zResources.zip -q --show-progress
+        elif [[ $Install_Grasscutter_Resources_Version = "2.7" ]]; then
+            wget https://gitlab.com/yukiz/GrasscutterResources/-/archive/2.7/GrasscutterResources-2.7.zip -O zResources.zip -q --show-progress
+        elif [[ $Install_Grasscutter_Resources_Version = "2.6" ]]; then
+            wget https://gitlab.com/yukiz/GrasscutterResources/-/archive/2.6/GrasscutterResources-2.6.zip -O zResources.zip -q --show-progress
+        fi
+    fi
+    echo -e "${GC}Unzip Resources Don't exit/kill or close termux...${WC}"
+    unzip $HOME/zResources.zip &> /dev/null
     echo "${GC}Move Resources to Grasscutter${WC}"
     sleep 1s
-    mv $HOME/Grasscutter_Resources/Resources $HOME/Grasscutter/resources
-    rm -rf $HOME/Grasscutter_Resources
+    mv $HOME/Grasscutter*Resources*/Resources $HOME/Grasscutter/resources
+    rm -rf $HOME/Grasscutter*Resources*
     cd $HOME/Grasscutter
     clear
     echo "${GC}Compile jar (Please Wait)${WC}"
@@ -646,17 +706,39 @@ Install_Grasscutter_process() {
 
 Install_Grasscutter_yes() {
     if [ -d "$HOME/Grasscutter-backup" ]; then
+        credit_hah
         echo "${RC}Can't do backup${YC}"
         echo "Because${WC} : $HOME/Grasscutter-backup exist"
         echo "${CCB}Remove manual for backup folder${WC}"
         echo
         echo -n "Press enter for continue remove Grasscutter folder and Install"
         read
-        Install_Grasscutter_process
+        Install_Grasscutter_option
     else
+        credit_hah
+        echo "${GC}Backup Grasscutter...${WC}"
         mv $HOME/Grasscutter $HOME/Grasscutter-backup
-        Install_Grasscutter_process
+        echo "${GC}Done...Now Install${WC}"
+        sleep 1s
+        Install_Grasscutter_option
     fi
+}
+
+Install_Grasscutter_option() {
+    credit_hah
+    echo "${GC}Please choose this for download Resources${WC}"
+    echo "${YC}1. Koko-Boya Resources"
+    echo "2. Yuuki Resources"
+    echo "0. ${RC}Back/Cancel${WC}"
+    echo
+    echo -n "Enter input : ${CCB}"
+    read Install_Grasscutter_option_input
+    case $Install_Grasscutter_option_input in
+        "1" ) Install_Grasscutter_Resources="Koko-Boya"; Install_Grasscutter_process;;
+        "2" ) Install_Grasscutter_Resources="Yuuki"; Install_Grasscutter_process;;
+        "0" ) main_menu;;
+        * ) echo "${RC}Wrong input!${WC}"; sleep 1s; Install_Grasscutter_option;;
+    esac
 }
 
 Install_Grasscutter() {
@@ -700,7 +782,7 @@ main_menu() {
 
 newVersionScript=""
 source <(curl -s https://raw.githubusercontent.com/ElaXan/GCAndroid/main/updateinfo)
-versionScript="2"
+versionScript="2.1"
 if [[ $newVersionScript = "" ]]; then
     clear
     note_credit="${RC}Can't connect to Server${WC}"
