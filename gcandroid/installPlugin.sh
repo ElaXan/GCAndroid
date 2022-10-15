71a14337ec23a4253c05f5c8b086d0de37e57a84
ElaXan <elashxander@gmail.com>
2022-10-15 17:59:29 +0700

    Add Dialog Command for selecting file

--- gcandroid/installPlugin.sh
+++ gcandroid/installPlugin.sh
@@ -68,37 +68,75 @@ installPlugin_Download() {
 
 installPlugin_from_directory() {
     credit_hah
-    echo "${YC}Enter b/B for back${WC}"
-    echo
-    echo "${CCB}Please enter where .jar plugin${WC}"
-    echo -n "${GC}Path : ${WC}"
-    read -r installPlugin_from_directory_Input
-    if [[ $installPlugin_from_directory_Input == "" ]]; then
-        echo "${RC}Please enter path!${WC}"
+    echo "1. ${CCB}Using dialog command${WC}"
+    echo "2. ${CCB}Manual Typing${WC}"
+    echo "3. ${CCB}Help for Dialog command${WC}"
+    echo "0. ${RC}Back${WC}"
+    echo -n "Enter input : "
+    read -r installPlugin_from_directory_input
+    case $installPlugin_from_directory_input in
+        "1" ) installPlugin_from_directory_Input="1"; installPlugin_from_directory_process;;
+        "2" ) installPlugin_from_directory_Input="2"; installPlugin_from_directory_process;;
+        "3" ) credit_hah
+                echo "${YC}1. ${CCB}Space on your keyboard for select file!${WC}"
+                echo "${YC}2. ${CCB}Use ↑, ↓, ←, → for interaction!${WC}"
+                echo "${YC}3. ${CCB}Enter on your keyboard to continue if the choice is correct${WC}"
+                echo
+                echo -n "Press enter for back!"
+                read
+                installPlugin_from_directory;;
+        "0" ) installPlugin;;
+        * ) echo "${RC}Wrong input!${WC}"; sleep 1s; installPlugin_from_directory;;
+    esac
+}
+
+installPlugin_from_directory_process() {
+    if [[ $installPlugin_from_directory_Input == "1" ]]; then
+        FILE=$(dialog --stdout --title "Please choose a .jar plugin" --fselect /sdcard/ 14 48)
+        clear
+        installPlugin_from_directory_Input_File=$FILE
+        # echo "$installPlugin_from_directory_Input_File"
+        # exit
+        credit_hah
+    elif [[ $installPlugin_from_directory_Input == "2" ]]; then
+        credit_hah
+        echo "${YC}Enter b/B for back${WC}"
+        echo
+        echo "${CCB}Please enter where .jar plugin${WC}"
+        echo -n "${GC}Path : ${WC}"
+        read -r installPlugin_from_directory_Input_File
+    else
+        echo "${RC}Wrong input!${WC}"
         sleep 1s
         installPlugin_from_directory
         return
-    elif [[ $installPlugin_from_directory_Input == "b" ]] || [[ $installPlugin_from_directory_Input == "B" ]]; then
-        installPlugin
+    fi
+    if [[ $installPlugin_from_directory_Input_File == "" ]]; then
+        echo "${RC}Please enter path!${WC}"
+        sleep 1s
+        installPlugin_from_directory_process
+        return
+    elif [[ $installPlugin_from_directory_Input_File == "b" ]] || [[ $installPlugin_from_directory_Input == "B" ]]; then
+        installPlugin_from_directory
         return
     fi
-    if ! [ -f "$installPlugin_from_directory_Input" ]; then 
-        echo "${RC}.jar not found! in ${installPlugin_from_directory_Input}${WC}"
+    if ! [ -f "$installPlugin_from_directory_Input_File" ]; then 
+        echo "${RC}.jar not found! in ${installPlugin_from_directory_Input_File}${WC}"
         sleep 1.5s
         installPlugin_from_directory
         return
     else
-        if ! [ $(ls $installPlugin_from_directory_Input | sed "s/.*\.//g") == "jar" ]; then
-            echo "${RC}Cannot continue because $installPlugin_from_directory_Input not .jar file!${WC}"
+        if ! [ "$(ls $installPlugin_from_directory_Input_File | sed "s/.*\.//g")" == "jar" ]; then
+            echo "${RC}Cannot continue because $( echo "$installPlugin_from_directory_Input_File" | sed "s/.*\///g") not .jar file!${WC}"
             echo
             read -p "Press enter for back to Menu!"
             main_menu
             return
         else
-            cp "$installPlugin_from_directory_Input" $HOME/Grasscutter/plugins &>/dev/null
+            cp "$installPlugin_from_directory_Input_File" $HOME/Grasscutter/plugins &>/dev/null
         fi
-        getNamePlugin=$(ls $installPlugin_from_directory_Input | sed "s/.*\///g")
-        if [[ ! -f "$HOME/Grasscutter/plugins/$(echo $installPlugin_from_directory_Input | sed "s/.*\///g")" ]]; then
+        getNamePlugin=$(ls $installPlugin_from_directory_Input_File | sed "s/.*\///g")
+        if [[ ! -f "$HOME/Grasscutter/plugins/$(echo $installPlugin_from_directory_Input_File | sed "s/.*\///g")" ]]; then
             echo "${RC}Failed install ${getNamePlugin} plugin to Grasscutter Folder!${WC}"
             echo
             read -p "Press enter for back to Menu!"
@@ -127,7 +165,7 @@ installPlugin() {
     echo "1. ${CCB}Download plugin and Install${WC}"
     echo "2. ${CCB}Install Plugin from local directory${WC}"
     echo "0. ${RC}Back${WC}"
-    echo -n "Enter inpur : "
+    echo -n "Enter input : "
     read -r installPlugin_Input
     case $installPlugin_Input in
         "1" ) installPlugin_Download;;
