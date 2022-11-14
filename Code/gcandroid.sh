2007fa77c561dfebd51c80d1d1b9115a6815ae7e
ElaXan <zero@elaxan.com>
2022-11-14 19:14:59 +0700

    Add edit config.json for "sceneEntityLimit"

--- Code/gcandroid.sh
+++ Code/gcandroid.sh
@@ -341,6 +341,13 @@ else
     exit 1
 fi
 
+if [ -f "$GCAndroid/Edit_Config_Json/Server/game/gameOptions/sceneEntityLimit.sh" ]; then
+    source $GCAndroid/Edit_Config_Json/Server/game/gameOptions/sceneEntityLimit.sh
+else
+    echo "${RC}Error${WC} : ${GCAndroid}/Edit_Config_Json/Server/game/gameOptions/sceneEntityLimit.sh not found!"
+    exit 1
+fi
+
 configpath=$HOME/Grasscutter/config.json
 wherethegrassss=$HOME/Grasscutter/grasscutter.jar
 inpscript=$1
