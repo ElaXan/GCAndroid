f5987a14eb0b097178d6c6acbe55a16cbdbecc3c
ElaXan <zero@elaxan.com>
2023-09-14 22:20:48 +0700

    feat: update `import.bat` for improved functionality

--- import.bat
+++ import.bat
@@ -1,6 +1,7 @@
 @echo off
 cd ..
-scp -r GCAndroid Termux:~/.ElaXan
+ssh Termux -- mkdir $HOME/.ElaXan
+scp -r GCAndroid Termux:~/.ElaXan/GCAndroid
 ssh Termux -- ln -sv $HOME/.ElaXan/GCAndroid/Code/gcandroid.sh $PREFIX/bin/gcandroid
 ssh Termux -- chmod +x $PREFIX/bin/gcandroid
 ssh Termux -- bash $HOME/.ElaXan/GCAndroid/dos2unix.sh
