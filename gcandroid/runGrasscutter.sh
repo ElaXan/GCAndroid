d0a2ce7117ff176f30a38a9730697ccbe57cfd0c
ElaXan <zero@elaxan.com>
2023-09-14 22:24:32 +0700

    feat: remove port checking and plan to replace it with a better solution

--- gcandroid/runGrasscutter.sh
+++ gcandroid/runGrasscutter.sh
@@ -22,25 +22,6 @@ GoTouchGrass() {
         return
     else
         config_file="$grasscutter_path/config.json"
-        if [[ -f $config_file ]]; then
-            access_address=$(jq -r .server.http.accessAddress $config_file)
-            bind_port=$(jq .server.http.bindPort $config_file)
-
-            echo "${GC}Checking port used${WC}"
-            timeout --foreground 2s php -S $access_address:$bind_port &>/dev/null
-            exitCode=$?
-            if [[ $exitCode == 1 ]]; then
-                echo "${RC}Error${WC} : Port $bind_port is used/permission denied!"
-
-                echo "${YC}Server: $access_address:$bind_port${WC}"
-                echo
-                echo -n "Press enter for back to Menu!"
-                read -r
-                main_menu
-                return
-            fi
-            echo "${GC}Port $bind_port is not used!${WC}"
-        fi
         if pgrep mongod > /dev/null; then
             pkill mongod
         fi
