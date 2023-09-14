479ada07475db673d3b5470fa0fd394b8f1404fa
ElaXan <zero@elaxan.com>
2023-09-14 22:21:53 +0700

    chore: remove Python files from gcandroid/src

--- gcandroid/src/README.md
+++
@@ -1,3 +0,0 @@
-# GCAndroid
-
-This code is only stored here first, because it is not yet stable or needs to be fixed a lot, before using it on the project
--- gcandroid/src/color.py
+++
@@ -1,9 +0,0 @@
-
-
-class color:
-    def __init__(self) -> None:
-        self.green = "\033[1;32m"
-        self.red = "\033[1;31m"
-        self.white = "\033[0;37m"
-        self.magenta = "\033[1;35m"
-        
