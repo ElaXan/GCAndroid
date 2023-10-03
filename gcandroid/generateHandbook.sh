2523c9daf908066a09c86a65f5a1fe2a81710431
ElaXan <zero@elaxan.com>
2023-10-04 00:48:47 +0700

    feat: Improve GMHandbook for new version

--- gcandroid/generateHandbook.sh
+++ gcandroid/generateHandbook.sh
@@ -11,27 +11,6 @@ generateHandbook() {
         read -r
         Grasscutter_Tools
     fi
-    if ! command -v mongo &>/dev/null; then
-        echo "${RC}Mongodb not installed, install it in Main Menu!${RC}"
-        echo
-        echo -n "Press enter for back to Main Menu!"
-        read -r
-        main_menu
-    fi
-    if [ ! -d "$grasscutter_path" ]; then   
-        echo "${RC}Folder $nameFolder not found!${RC}"
-        echo
-        echo -n "Press enter for back to Grasscutter Tools"
-        read -r
-        Grasscutter_Tools
-    fi
-    if [ ! -f "$grasscutter_path/grasscutter.jar" ]; then
-        echo "${RC}File grasscutter.jar not found!${RC}"
-        echo
-        echo -n "Press enter for back to Grasscutter Tools"
-        read -r
-        Grasscutter_Tools
-    fi
     if ! (ls /sdcard > /dev/null 2>&1); then
         echo "${RC}Error${WC} : /sdcard can't access/permission denied"
         echo
@@ -39,21 +18,34 @@ generateHandbook() {
         read -r
         Grasscutter_Tools
     fi
-    cd $grasscutter_path || exit 1
-    mongod &>/dev/null &
-    Run "timeout 10s java -jar grasscutter.jar -handbook" "Getting GM_Handbook" "124" "Menu" "main_menu"
-    if [ -z "$(ls -A $grasscutter_path/GM\ Handbook/)" ]; then
-        echo "${RC}Error${WC} : GM Handbook not found!"
+    if [ ! -d "$grasscutter_path" ]; then
+        echo "${RC}Error${WC} : $grasscutter_path not found${WC}"
         echo
         echo -n "Press enter for back to Grasscutter Tools"
         read -r
         Grasscutter_Tools
     fi
+    if [ ! -f "$wherethegrassss" ]; then
+        echo "${RC}Error${WC} : $wherethegrassss file not found${WC}"
+        echo
+        echo -n "Press enter for back to Grasscutter Tools"
+        read -r
+        Grasscutter_Tools
+    fi
+    cd $grasscutter_path || exit 1
+    mongod &>/dev/null &
+    local command_jar="java -jar grasscutter.jar"  
+    Run "$command_jar -dump=avatars,EN" "Dump Avatars with language EN" "0" "Menu" "main_menu"
+    Run "$command_jar -dump=items,EN" "Dump Items with language EN" "0" "Menu" "main_menu"
+    Run "$command_jar -dump=commands,en-us" "Dump Commands with language en-us" "0" "Menu" "main_menu"
+    Run "$command_jar -dump=quests,EN" "Dump Quests with language EN" "0" "Menu" "main_menu"
+    Run "$command_jar -dump=entities,EN" "Dump Entities with language EN" "0" "Menu" "main_menu"
+    Run "$command_jar -dump=areas,EN" "Dump Areas with language EN" "0" "Menu" "main_menu"
+    Run "$command_jar -dump=scenes,EN" "Dump Scenes with language EN" "0" "Menu" "main_menu"
     if [ ! -d "/sdcard/GM_Handbook" ]; then
         mkdir "/sdcard/GM_Handbook"
     fi
-    Run "mv -f GM*Handbook/* /sdcard/GM_Handbook/" "Move GM_Handbook to /sdcard" "0" "Menu" "main_menu"
-    pkill mongod
+    Run "mv -t /sdcard/GM_Handbook "*.csv "Move GM Handbook to /sdcard" "0" "Menu" "main_menu"
     if (ls "/sdcard/GM_Handbook"/* > /dev/null 2>&1); then
         echo "${GC}Success get GM Handbook to /sdcard in Folder GM_Handbook${WC}"
         echo
