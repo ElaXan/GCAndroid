d34ac70cb7476185b82db49f8d2587ee287b6abd
ElaXan <zero@elaxan.com>
2023-10-12 11:42:44 +0000

    docs: Add JSDoc comments to Logger class and ColorCode enum

--- src/Utils/log.ts
+++ src/Utils/log.ts
@@ -1,24 +1,65 @@
+/**
+ * Enum representing color codes.
+ */
 enum ColorCode {
+    /**
+     * Reset color code.
+     */
     Reset = '\x1b[0m',
+    /**
+     * Green color code.
+     */
     Green = '\x1b[32m',
+    /**
+     * Red color code.
+     */
     Red = '\x1b[31m',
+    /**
+     * Yellow color code.
+     */
     Yellow = '\x1b[33m'
 }
 
 export default class Logger {
+    /**
+     * Logs a message with the specified log level and color code.
+     *
+     * @param {string} level - The log level.
+     * @param {ColorCode} colorCode - The color code.
+     * @param {...any[]} message - The message to be logged.
+     * @return {void}
+     */
     private static log(level: string, colorCode: ColorCode, ...message: any[]): void {
         const formattedMessage = `[${colorCode}${level}${ColorCode.Reset}]:`;
         console.log(formattedMessage, ...message);
     }
 
+    /**
+     * Logs an informational message.
+     *
+     * @param {...any[]} message - The message to be logged.
+     * @return {void} - This function does not return anything.
+     */
     public static info(...message: any[]): void {
         this.log('INFO', ColorCode.Green, ...message)
     }
 
+    /**
+     * Prints an error message to the console.
+     *
+     * @param {...any[]} message - The error message to be printed.
+     * @return {void} - This function does not return anything.
+     */
     public static error(...message: any[]): void {
         this.log('ERROR', ColorCode.Red, ...message)
     }
 
+    /**
+     * Logs a warning message.
+     *
+     * @param {...any[]} message - The message(s) to be logged.
+     * @return {void} - This function does not return anything.
+     */
     public static warn(...message: any[]): void {
         this.log('WARN', ColorCode.Yellow, ...message);
     }
