251e8bccf003f05e5a3a53a09513e6241b09e09e
ElaXan <zero@elaxan.com>
2023-10-12 11:41:55 +0000

    docs: Add JSDoc comments to JSONUtility class methods

--- src/Utils/Json.ts
+++ src/Utils/Json.ts
@@ -4,11 +4,25 @@ export default class JSONUtility {
     private path: string;
     private keyPath: string;
 
+    /**
+     * Creates a new instance of the constructor.
+     *
+     * @param {Object} options - The options for the constructor.
+     * @param {string} options.path - The path parameter.
+     * @param {string} options.keyPath - The keyPath parameter.
+     */
     constructor(options: { path: string, keyPath: string }) {
         this.path = options.path;
         this.keyPath = options.keyPath;
     }
 
+    /**
+     * Reads the contents of a JSON file located at the specified path
+     * and returns the value at the specified key path.
+     *
+     * @return {any} The value at the specified key path in the JSON file.
+     * @throws {Error} If the key path is invalid or if an error occurs while reading the file.
+     */
     public read(): any {
         try {
             const jsonData = JSON.parse(fs.readFileSync(this.path, 'utf-8'))
@@ -30,6 +44,12 @@ export default class JSONUtility {
         }
     }
 
+    /**
+     * Writes a new value to a JSON file.
+     *
+     * @param {any} newValue - The new value to be written.
+     * @return {void} This function does not return anything.
+     */
     public write(newValue: any): void {
         try {
             const jsonData = JSON.parse(fs.readFileSync(this.path, 'utf-8'))
