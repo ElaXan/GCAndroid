e2676967d5527f5ad6ed9487176052a22e235bc5
ElaXan <zero@elaxan.com>
2023-11-03 18:16:07 +0700

    refactor(shell.ts): restructure shell utility class for better organization and readability feat(shell.ts): add support for optional exit code and onDataCallback in shell.execute() method to provide more flexibility in command execution feat(shell.ts): add checkCommand() method to check if a command is available before executing it

--- src/Utils/shell.ts
+++ src/Utils/shell.ts
@@ -1,60 +1,88 @@
 import { spawn } from 'child_process'
 import Logger from './log';
 
-/**
-* Executes a shell command asynchronously.
-*
-* @param {string} command - The shell command to execute.
-* @param {number} errorCode - The expected exit code of the command.
-* @param {Function} onDataCallback - A callback function to handle the command output.
-* @return {Promise<void>} A promise that resolves when the command completes successfully, or rejects with an error message.
-*/
-export async function shell(command: string, errorCode: number, onDataCallback: (data: string) => void): Promise<void> {
-    let output = ''
-    return new Promise<void>((resolve, reject) => {
-        const cloneProcess = spawn(command, { shell: true });
-        cloneProcess.stdout?.on('data', (data) => {
-            output += data.toString()
-            onDataCallback(data.toString());
-        })
+class Shell {
+    /**
+     * Executes a shell command asynchronously. Default exit code is 0.
+     * @param {string} command - The shell command to execute.
+     * @return {Promise<string>} A promise that resolves when the command completes successfully, or rejects with an error message.
+     */
+    public static async execute(command: string): Promise<string>;
+    /**
+     * Executes a shell command asynchronously.
+     * @param {string} command - The shell command to execute.
+     * @param {number} exitCode - The expected exit code of the command.
+     * @return {Promise<string>} A promise that resolves when the command completes successfully, or rejects with an error message.
+     */
+    public static async execute(command: string, exitCode: number): Promise<string>;
+    /**
+    * Executes a shell command asynchronously.
+    *
+    * @param {string} command - The shell command to execute.
+    * @param {number} errorCode - The expected exit code of the command.
+    * @param {Function} onDataCallback - A callback function to handle the command output.
+    * @return {Promise<void>} A promise that resolves when the command completes successfully, or rejects with an error message.
+    */
+    public static async execute(command: string, exitCode: number, onDataCallback: (data: string) => void): Promise<string>;
 
-        cloneProcess.stderr?.on('data', (data) => {
-            output += data.toString()
-            onDataCallback(data.toString());
-        })
+    public static async execute(command: string, exitCode?: number | undefined, onDataCallback?: (data: string) => void): Promise<string | void> {
+        let output = '';
+        return new Promise<string | void>((resolve, reject) => {
+            const child = spawn(command, { shell: true });
+            child.stdout.on('data', (data) => {
+                output += data.toString();
+                if (onDataCallback) {
+                    onDataCallback(data.toString());
+                }
+            })
 
-        cloneProcess.on('close', (code) => {
-            const message = 'Program exited with code'
-            if (code === errorCode) {
-                resolve()
-            } else {
-                reject(`${output}\n${message} ${code}`)
-            }
+            child.stderr.on('data', (data) => {
+                output += data.toString();
+                if (onDataCallback) {
+                    onDataCallback(data.toString());
+                }
+            })
+
+            child.on('close', (code) => {
+                const message = 'Program exited with code'
+                const exit = exitCode || 0;
+                if (code === exit) {
+                    if (!onDataCallback) {
+                        resolve(output)
+                    } else {
+                        resolve()
+                    }
+                } else {
+                    reject(`${output}\n${message} ${code}`)
+                }
+            })
         })
-    })
+    }
+
+    /**
+    * Checks if a command is available.
+    *
+    * @param {string} command - The command to check availability for.
+    * @return {Promise<boolean>} A promise that resolves to a boolean value indicating if the command is available.
+    */
+    public static async checkCommand(command: string): Promise<boolean> {
+        return new Promise<boolean>((resolve) => {
+            const commandName = process.platform === 'win32' ? 'where' : 'command -v';
+            const childProcess = spawn(commandName, [command], {
+                shell: true
+            })
+
+            childProcess.on('exit', (code) => {
+                const isAvailable = code === 0;
+                resolve(isAvailable)
+            })
+
+            childProcess.on('error', (err) => {
+                Logger.error(err)
+                resolve(false)
+            })
+        })
+    }
 }
 
-/**
- * Checks if a command is available.
- *
- * @param {string} command - The command to check availability for.
- * @return {Promise<boolean>} A promise that resolves to a boolean value indicating if the command is available.
- */
-export async function isCommandAvailable(command: string): Promise<boolean> {
-    return new Promise<boolean>((resolve) => {
-        const commandName = process.platform === 'win32' ? 'where' : 'command -v';
-        const childProcess = spawn(commandName, [command], {
-            shell: true
-        })
-
-        childProcess.on('exit', (code) => {
-            const isAvailable = code === 0;
-            resolve(isAvailable)
-        })
-
-        childProcess.on('error', (err) => {
-            Logger.error(err)
-            resolve(false)
-        })
-    })
-}
+export default Shell;
